bb078aaeb0b0f5c739af81012b55407eae97f280
Dranzer <cared@DRANZA.COM>
2022-01-06 18:37:02 -0500

    validation body

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ app = FastAPI() # Create an app instance.
 
 #models
 
+class Location(BaseModel):
+    city : str
+    state : str
+    country : str
+
 class Person(BaseModel):
     first_name: str
     last_name: str
@@ -63,4 +68,21 @@ def show_person (
 ):
     return {person_id:'it exist'}
 
+#Request Body
 
+@app.put('/person/{person_id}')
+def update_person(
+        person_id: int = Path(
+            ...,
+            title='Person ID',
+            description='this is the person ID',
+            gt=0
+        ),
+        person: Person = Body(...),
+        location: Location = Body(...)
+):
+    results = person.dict()
+    results.update(location.dict())
+    return results
+
+#Request Body validarions
