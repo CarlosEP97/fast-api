53882d63b7af2142fe7e91de303b9c7df3475a70
Dranzer <cared@DRANZA.COM>
2022-01-08 15:15:10 -0500

    automatic examples

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #Python
-from typing import Optional,List
+from typing import Optional,List,Dict
 from enum import Enum
 #Pydantic
 from pydantic import BaseModel
 from pydantic import Field
+from pydantic import EmailStr,HttpUrl
 #fastAPI
 from fastapi import FastAPI # Import FastAPI.
 from fastapi import Body,Query,Path
@@ -19,18 +20,40 @@ class HairColor(Enum):
     yellow = 'yellow'
     red = 'red'
 
+class city(Enum):
+    Bogota = 'bogota'
+    Medellin = 'Medellin'
+    Bucaramanga = 'bucaramanga'
+    Barranquilla = 'barranquilla'
+
 class Location(BaseModel):
-    city : str
-    state : str
-    country : str
+    city: str = Field(..., min_length=1, max_length=50)
+    state: str = Field(..., min_length=1, max_length=50)
+    country: str = Field(..., min_length=1, max_length=50)
 
 class Person(BaseModel):
     first_name: str = Field(...,min_length=1,max_length=50)
     last_name: str = Field(...,min_length=1,max_length=50)
     age: int = Field(...,gt=0,le=100)
+    email: str = EmailStr(...)
+    url: HttpUrl
     hair_color: Optional[HairColor] = Field(default=None)
     is_married: Optional[bool] = Field(default=None)
 
+    # class Config:
+    #     schema_extra = {
+    #         "example": {
+    #             "first_name": "Facundo",
+    #             "last_name": "García Martoni",
+    #             "age": 21,
+    #             "hair_color": "blonde",
+    #             "is_married": False
+    #         }
+    #     }  data for autoComplete
+
+class Net_info(BaseModel):
+    email: str = EmailStr(...)
+    url: HttpUrl
 
 
 @app.get('/')
@@ -94,6 +117,46 @@ def update_person(
     results.update(location.dict())
     return results
 
+
 #Request Body validarions
 
 #Se hace en la definicion del modelo arriba
+
+# @app.post('/person/{email}')
+# def email(
+#         email : str = Path(
+#             ...,
+#             title='Correo electronico',
+#             description='email del usuario',
+#             min_length=15,
+#             max_length=50
+#         ),
+#         name: Optional[str] = Path(
+#             default=None,
+#             min_length=1,
+#             max_length=50,
+#             title='Person Name',
+#             description='this is person name,its between 1 and 50 char'
+#         )
+#
+# ):
+#     return {'email':email}
+
+# @app.post('/Net_info/new')
+# def create_mail(net_info: Net_info = Body(...)):
+#     return net_info
+
+
+@app.post('/Net_info/{new}')
+def email(
+        new: str = Path(
+            ...,
+            title='Person ID',
+            description='this is the person ID',
+
+        ),
+        net_info: Net_info = Body(...)
+
+):
+    result = net_info.dict()
+    return result
